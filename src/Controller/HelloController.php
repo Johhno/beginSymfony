3d2d0bac1229694b8eefd529fb586b50ed70c662
jono <johntchen.mos@gmail.com>
2021-04-06 11:10:24 +1100

    construct Logger

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,19 +3,25 @@
 namespace App\Controller;
 
 
+// use App\Taxes\Calculator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class HelloController
 {
-
+    protected $logger;
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     /** @Route(
      * "/hello/{prenom?World}",
      * name ="hello")
      */
     public function hello($prenom)
     {
+        $this->logger->error('Mon meessage de log');
         return new Response("Hello $prenom");
     }
 }
