c07070c620410ed08d724f513802b58cddde605d
jono <johntchen.mos@gmail.com>
2021-04-12 18:08:48 +1100

    base pratiques render()

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -9,14 +9,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
-    /** @Route(
-     * "/hello/{prenom?World}",
-     * name ="hello")
-     */
+
+    /* Ces 2 fonctions ont en commun : twig , render(). */
+    /** @Route(  "/hello/{prenom?World}", name ="hello") */
     public function hello($prenom, Environment $twig)
     {
         $html = $twig->render('hello.html.twig', [
             'prenom' => $prenom
+        ]);
+        return new Response($html);
+    }
+
+    /** @Route( * "/example", name ="example") */
+    public function example(Environment $twig)
+    {
+        $html = $twig->render('example.html.twig', [
+            'age' => 33
         ]);
         return new Response($html);
     }
