60918f2e90b7ed0cc7e5750c362f92ae0ac3d333
jono <johntchen.mos@gmail.com>
2021-04-12 17:13:11 +1100

    base pratiques render()

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -16,10 +16,7 @@ class HelloController
     public function hello($prenom, Environment $twig)
     {
         $html = $twig->render('hello.html.twig', [
-            'prenom' => $prenom,
-            'formateur1'=>['prenom'=>'John','nom' => 'Tchen'],
-            'formateur2'=>['prenom'=>'Melodie','nom' => 'Huynh'],
-            'formateur3'=>['prenom'=>'Remy','nom' => 'Marquis']
+            'prenom' => $prenom
         ]);
         return new Response($html);
     }
