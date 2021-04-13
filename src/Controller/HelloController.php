8f97832ca8777422aacdc2f7c0c4178dbaf63db9
jono <johntchen.mos@gmail.com>
2021-04-13 22:22:19 +1100

    homepage

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,20 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Twig\Environment;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
 
-    /** @Route(  "/hello/{prenom?World}", name ="hello") */
+    /** @Route(
+     *"/hello/{prenom?World}", 
+     * name ="hello") */
     public function hello($prenom)
     {
         return $this->render('hello.html.twig', ['prenom' => $prenom]);
     }
 
-    /** @Route( * "/example", name ="example") */
+    /** @Route( 
+     *"/example", 
+     * name ="example") */
     public function example()
     {
         return $this->render('example.html.twig', ['age' => 33]);
